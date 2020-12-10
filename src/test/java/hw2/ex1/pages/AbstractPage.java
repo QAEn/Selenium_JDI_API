8555a4656b1413f0aff1ba235bdd31a9fee796c5
Aleksei Lysogora <L_AV@yahoo.com>
2020-12-10 13:56:11 +0300

    Change Abstract classes

--- src/test/java/hw2/ex1/pages/AbstractPage.java
+++ src/test/java/hw2/ex1/pages/AbstractPage.java
@@ -1,6 +1,34 @@
 package hw2.ex1.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public abstract class AbstractPage {
     public abstract void openPage();
     public abstract boolean checkTitle();
+
+    public WebElement waitForElementLocatedBy(WebDriver driver, By by) {
+        return new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    public String getPageProperty(String nameOfProperty) {
+
+        final Properties property = new Properties();
+        try (final FileInputStream fis = new FileInputStream("src/test/resources/hw2/properties/exercise_1.properties"))
+        {
+            property.load(fis);
+        } catch (FileNotFoundException e) {
+            System.out.println("File with resources of page was not founded (exercise 2)");
+        } catch (IOException e) {
+            System.out.println("IOException for page (exercise 2)");
+        }
+        return property.getProperty(nameOfProperty);
+    }
 }
--- src/test/java/hw2/ex1/pages/HomePage.java
+++ src/test/java/hw2/ex1/pages/HomePage.java
@@ -286,21 +286,4 @@ public class HomePage extends AbstractPage {
             Assert.assertEquals(actual, expected);
         }
     }
-
-    private WebElement waitForElementLocatedBy(WebDriver driver, By by) {
-        return new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(by));
-    }
-
-    public String getPageProperty(String nameOfProperty) {
-        final Properties property = new Properties();
-        try (final FileInputStream fis = new FileInputStream("src/test/resources/hw2/properties/exercise_1.properties"))
-        {
-            property.load(fis);
-        } catch (FileNotFoundException e) {
-            System.out.println("File with resources of page was not founded");
-        } catch (IOException e) {
-            System.out.println("IOException for page");
-        }
-        return property.getProperty(nameOfProperty);
-    }
 }
