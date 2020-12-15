bd691fb73dff150b43688a99b14bdcc6b454ff8e
Aleksei Lysogora <L_AV@yahoo.com>
2020-12-15 15:33:53 +0300

    use pure selenium in the @Test methods

--- src/test/java/hw2/ex1/runtest/RunTest.java
+++ src/test/java/hw2/ex1/runtest/RunTest.java
@@ -1,64 +1,199 @@
 package hw2.ex1.runtest;
 
-import hw2.baseclass.DriverManager;
-import hw2.ex1.pages.HomePage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class RunTest {
 
     private WebDriver driver;
-    private HomePage homePage;
+    private SoftAssert softAssertion;
+    private WebDriverWait wait;
 
     @BeforeMethod(alwaysRun = true)
     public void browserSetup() {
-        driver =  new DriverManager().setupDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, 10);
+        softAssertion = new SoftAssert();
     }
 
     @Test(
             description = "First exercise test, Jira binding cab be here"
     )
     public void exercise_1_Test() {
-        homePage = new HomePage(driver);
-
         //STEP #1: Open test site by URL
-        homePage.openPage();
+        driver.get("https://jdi-testing.github.io/jdi-light/index.html");
+        wait.until(ExpectedConditions.presenceOfElementLocated((By.tagName("html"))));
 
         //STEP #2: Assert Browser title
-        homePage.checkTitle();
+        softAssertion.assertEquals(driver.getTitle(), "Home Page",
+                "Incorrect Home page title");
 
         //STEP #3: Perform login
-        homePage.login();
+        WebElement loginCaret = driver.findElement(By.xpath("//a[contains(@href, '#')]"));
+        loginCaret.click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#name")));
+        WebElement username = driver.findElement(By.cssSelector("#name"));
+        username.sendKeys("Roman");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#password")));
+        WebElement pass = driver.findElement(By.cssSelector("#password"));
+        pass.sendKeys("Jdi1234");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='Enter']")));
+        WebElement enterBtn = driver.findElement(By.xpath("//*[text()='Enter']"));
+        enterBtn.click();
 
         //STEP #4: Assert Username is logged
-        homePage.assertUserName();
+        WebElement userName = driver.findElement(By.cssSelector("#user-name"));
+        softAssertion.assertEquals(userName.getText(), "ROMAN IOVLEV",
+                "Incorrect username");
 
         //STEP #5: Assert that there are 4 items on the header section
         //         are displayed and they have proper texts
-        homePage.assertHeaderItems();
+
+        //assert "HOME"
+        WebElement home = driver.findElement(By.xpath("//a[contains(text(),'Home')]"));
+        softAssertion.assertEquals(home.getText(), "HOME",
+                "Incorrect HOME item on the header section");
+
+        //assert "CONTACT FORM"
+        WebElement contactForm = driver.findElement(
+                By.xpath("//a[contains(text(),'Contact form')]"));
+        softAssertion.assertEquals(contactForm.getText(), "CONTACT FORM",
+                "Incorrect CONTACT FORM item on the header section");
+
+        //assert "SERVICE"
+        WebElement service = driver.findElement(By.xpath("//a[contains(text(),'Service')]"));
+        softAssertion.assertEquals(service.getText(), "SERVICE",
+                "Incorrect SERVICE item on the header section");
+
+        //"METALS & COLORS"
+        WebElement metalColors = driver.findElement(
+                By.xpath("//a[contains(text(),'Metals & Colors')]"));
+        softAssertion.assertEquals(metalColors.getText(), "METALS & COLORS",
+                "Incorrect METALS & COLORS item on the header section");
 
         //STEP #6: Assert that there are 4 images on the Index Page and they are displayed
-        homePage.assertImages();
+        //microscopeImg
+        WebElement microscopeImg = driver.findElement(By.cssSelector(".icon-practise"));
+        softAssertion.assertTrue(microscopeImg.isDisplayed(),
+                "Microscope image is not displayed");
+
+        //headphonesImg
+        WebElement headphonesImg = driver.findElement(By.cssSelector(".icon-custom"));
+        softAssertion.assertTrue(headphonesImg.isDisplayed(),
+                "Headphones image is not displayed");
+
+        //monitorImg
+        WebElement monitorImg = driver.findElement(By.cssSelector(".icon-multi"));
+        softAssertion.assertTrue(monitorImg.isDisplayed(),
+                "Monitor image is not displayed");
+
+        //rocketImg
+        WebElement rocketImg = driver.findElement(By.cssSelector(".icon-base"));
+        softAssertion.assertTrue(rocketImg.isDisplayed(),
+                "Rocket image is not displayed");
 
         //STEP #7: Assert that there are 4 texts on the Index Page
         //         under icons and they have proper text
-        homePage.assertText();
+        //microscopeTxt
+        WebElement microscopeTxt = driver.findElement(
+                By.xpath("//span[contains(text(),'To include good practices')]"));
+        softAssertion.assertEquals(microscopeTxt.getText(),
+                "To include good practices\n"
+                        + "and ideas from successful\n"
+                        + "EPAM project",
+                "Microscope text is not displayed under icon");
+
+        //headphonesTxt
+        WebElement headphonesTxt = driver.findElement(
+                By.xpath("//span[contains(text(),'To be flexible and')]"));
+        softAssertion.assertEquals(headphonesTxt.getText(),
+                "To be flexible and\n"
+                        + "customizable",
+                "Headphone text is not displayed under icon");
+
+        //monitorTxt
+        WebElement monitorTxt = driver.findElement(
+                By.xpath("//span[contains(text(),'To be multiplatform')]"));
+        softAssertion.assertEquals(monitorTxt.getText(),
+                "To be multiplatform",
+                "Monitor text is not displayed under icon");
+
+        //rocketTxt
+        WebElement rocketTxt = driver.findElement(
+                By.xpath("//span[contains(text(),'Already have good base')]"));
+        softAssertion.assertEquals(rocketTxt.getText(),
+                "Already have good base\n"
+                        + "(about 20 internal and\n"
+                        + "some external projects),\n"
+                        + "wish to get more…",
+                "Rocket text is not displayed under icon");
 
         //STEP #8: Assert that there is the iframe with “Frame Button” exist
-        homePage.iframeWithButtonExists();
+        WebElement iframe = driver.findElement(By.xpath("//iframe[@id='frame']"));
+        Assert.assertEquals(iframe.getText(),
+                "<p>Your browser does not support iframes.</p>",
+                "The iframe with “Frame Button” isn't exist");
 
         //STEP #9: Switch to the iframe and check that there is “Frame Button” in the iframe
-        homePage.frameButtonExists();
+        WebElement switchIframe = driver.findElement(By.xpath("//iframe[@id='frame']"));
+        driver.switchTo().frame(switchIframe);
+        String iframeBtnStr = "//input[@id='frame-button']";
+        String actual = driver.findElement(By.xpath(iframeBtnStr)).getAttribute("value");
+        String expected = "Frame Button";
+        softAssertion.assertEquals(actual, expected);
 
         //STEP #10: Switch to original window back
-        homePage.switchHomePage();
-        homePage.checkTitle();
+        driver.switchTo().defaultContent();
+        softAssertion.assertTrue(headphonesImg.isDisplayed(),
+                "Switch to the original window back FAILED");
 
         //STEP #11: Assert that there are 5 items in the Left Section
         //          are displayed and they have proper text
-        homePage.assertLeftSectionItems();
+
+        //assert "Home"
+        WebElement homeLeftSide = driver.findElement(By.xpath("//span[contains(.,'Home')]"));
+        softAssertion.assertEquals(homeLeftSide.getText(), "Home",
+                "Home item in the left section is not displayed");
+
+        //assert "Contact form"
+        WebElement contactFormLeftSide = driver.findElement(
+                By.xpath("//span[contains(.,'Contact form')]"));
+        softAssertion.assertEquals(contactFormLeftSide.getText(), "Contact form",
+                "Contact form item is not displayed in the left section");
+
+        //assert "Service"
+        WebElement serviceLeftSide = driver.findElement(By.xpath("//span[contains(.,'Service')]"));
+        softAssertion.assertEquals(serviceLeftSide.getText(),"Service",
+                "Service form item is not displayed in the left section");
+
+        //"Metals & Colors"
+        WebElement metalColorsLeftSide = driver.findElement(
+                By.xpath("//span[contains(.,'Metals & Colors')]"));
+        softAssertion.assertEquals(metalColorsLeftSide.getText(), "Metals & Colors",
+                "Metals & Colors form item is not displayed in the left section");
+
+        // assert "Elements packs"
+        WebElement elementsPacksLeftSide = driver.findElement(
+                By.xpath("//span[contains(.,'Elements packs')]"));
+        softAssertion.assertEquals(elementsPacksLeftSide.getText(), "Elements packs",
+                "Elements packs form item is not displayed in the left section");
     }
 
     @AfterMethod(alwaysRun = true)
@@ -66,5 +201,6 @@ public class RunTest {
         //STEP #12: Close Browser
         driver.quit();
         driver = null;
+        softAssertion.assertAll();
     }
 }
--- src/test/java/hw2/ex2/runtest/RunTest.java
+++ src/test/java/hw2/ex2/runtest/RunTest.java
@@ -1,65 +1,136 @@
 package hw2.ex2.runtest;
 
-import hw2.baseclass.DriverManager;
-import hw2.ex2.pages.DifferentElementsPage;
-import hw2.ex2.pages.HomePage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
+import org.testng.asserts.SoftAssert;
 
 public class RunTest {
 
     private WebDriver driver;
-    private HomePage homePage;
-    private DifferentElementsPage differentElementsPage;
-
-    //    private String driverPath = getConfigProperty("driverPath");
-    //    private String typeOfWebDriver = getConfigProperty("typeOfWebDriver");
+    private SoftAssert softAssertion;
+    private WebDriverWait wait;
 
     @BeforeMethod(alwaysRun = true)
     public void browserSetup() {
-        driver =  new DriverManager().setupDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, 5);
+        softAssertion = new SoftAssert();
     }
 
     @Test(
             description = "Second exercise test, Jira binding cab be here"
     )
     public void exercise_2_Test() {
-        homePage = new HomePage(driver);
-        differentElementsPage = new DifferentElementsPage(driver);
 
         //STEP #1: Open test site by URL
-        homePage.openPage();
+        driver.get("https://jdi-testing.github.io/jdi-light/index.html");
+        wait.until(ExpectedConditions.presenceOfElementLocated((By.tagName("html"))));
 
         //STEP #2: Assert Browser title
-        homePage.checkTitle();
+        softAssertion.assertEquals(driver.getTitle(), "Home Page",
+                "Incorrect page title");
 
         //STEP #3: Perform login
-        homePage.login();
+        WebElement loginCaret = driver.findElement(By.xpath("//a[contains(@href, '#')]"));
+        loginCaret.click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#name")));
+        WebElement username = driver.findElement(By.cssSelector("#name"));
+        username.sendKeys("Roman");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#password")));
+        WebElement pass = driver.findElement(By.cssSelector("#password"));
+        pass.sendKeys("Jdi1234");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='Enter']")));
+        WebElement enterBtn = driver.findElement(By.xpath("//*[text()='Enter']"));
+        enterBtn.click();
 
         //STEP #4: Assert User name in the left-top side of screen that user is loggined
-        homePage.assertUserName();
+        WebElement userName = driver.findElement(By.cssSelector("#user-name"));
+        softAssertion.assertEquals(userName.getText(), "ROMAN IOVLEV",
+                "Incorrect username");
 
         //STEP #5: Open through the header menu Service -> Different Elements Page
-        differentElementsPage.openPage();
-        differentElementsPage.checkTitle();
+        //click Service header menu
+        WebElement serviceHeaderElement = driver.findElement(By.xpath("//li[3]/a[1]"));
+        serviceHeaderElement.click();
+
+        //click Different Elements
+        WebElement differentElements = driver.findElement(
+                By.xpath("//a[contains(@href, 'different-elements.html')]"));
+        differentElements.click();
+        wait.until(ExpectedConditions.titleContains("Different Elements"));
+
+        softAssertion.assertEquals(driver.getTitle(), "Different Elements",
+                "Incorrect Different Elements page title");
 
         //STEP #6: Select checkboxes
-        differentElementsPage.selectCheckboxes();
+        //select checkbox with parameter Water
+        WebElement water = driver.findElement(By.xpath("//input[@type='checkbox']"));
+        water.click();
+
+        //select checkbox with parameter Wind
+        WebElement wind = driver.findElement(By.xpath("//label[3]/input"));
+        wind.click();
 
         //STEP #7: Select radio
-        differentElementsPage.selectRadio();
+        WebElement selen = driver.findElement(By.xpath("//div[3]/label[4]/input"));
+        selen.click();
 
         //STEP #8: Select in dropdown
-        differentElementsPage.selectInDropdown();
+        WebElement dropdown = driver.findElement(By.xpath("//select"));
+        dropdown.click();
+        Select color = new Select(dropdown);
+        color.selectByVisibleText("Yellow");
 
         //STEP #9: Assert that each element corresponds to itself with positive status
-        differentElementsPage.assertActions();
+        //for each checkbox there is an individual log row and value
+        // is corresponded to the status of checkbox
+
+        //for Water
+        WebElement assertCheckBoxWater = driver.findElement(
+                By.xpath("//li[contains(text(),'Water: condition changed to true')]"));
+        String actualWater = assertCheckBoxWater.getText();
+        softAssertion.assertTrue(actualWater.contains("Water") && actualWater.endsWith("true"),
+                "Log row and (or) value isn't corresponded to the status of Water's checkbox");
+
+        //for Wind
+        WebElement assertCheckBoxWind = driver.findElement(
+                By.xpath("//li[contains(text(),'Wind: condition changed to true')]"));
+        String actualWind = assertCheckBoxWind.getText();
+        softAssertion.assertTrue(actualWind.contains("Wind") && actualWind.endsWith("true"),
+                "Log row and (or) value isn't corresponded to the status of Wind's checkbox");
+
+        //for radio button there is a log row and value
+        //is corresponded to the status of radio button
+        WebElement assertRadioBtn = driver.findElement(
+                By.xpath("//li[contains(text(),'Selen')]"));
+        String actualRadioBtn = assertRadioBtn.getText();
+        softAssertion.assertTrue(actualRadioBtn.contains("metal")
+                        && actualRadioBtn.endsWith("Selen"),
+                "Log row and (or) value isn't corresponded to the status of Selen's radio button");
+
+        //for dropdown there is a log row and value is corresponded to the selected value
+        WebElement assertDropdown = driver.findElement(
+                By.xpath("//li[contains(text(),'Color')]"));
+        String actualDropdown = assertDropdown.getText();
+        softAssertion.assertTrue(actualDropdown.contains("Colors")
+                        && actualDropdown.endsWith("Yellow"),
+                "Log row and (or) value isn't corresponded to the status of selected value");
     }
 
     @AfterMethod(alwaysRun = true)
@@ -67,5 +138,6 @@ public class RunTest {
         //STEP #10: Close Browser
         driver.quit();
         driver = null;
+        softAssertion.assertAll();
     }
 }
